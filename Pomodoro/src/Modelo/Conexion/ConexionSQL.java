8650d3315df31a80c7d3b82208f223a7e64272ae
Joaquín Castro <theeljoaquin@hotmail.com>
2022-10-24 21:58:42 -0700

    Conexión

--- Pomodoro/src/Modelo/Conexion/ConexionSQL.java
+++ Pomodoro/src/Modelo/Conexion/ConexionSQL.java
@@ -5,7 +5,7 @@
  */
 package Modelo.Conexion;
 
-import com.sun.jdi.connect.spi.Connection;
+import java.sql.*;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,17 +13,21 @@ import javax.swing.JOptionPane;
  * @author Carlos
  */
 public class ConexionSQL {
+
     Connection conectar = null;
     String USER = "root";
     String PASSWORD = "";
-    
-    public Connection conexion(){
+
+    public Connection conexion() throws SQLException, Exception {
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conectar = (Connection) DriverManager.getConnection("jdbc:mysql//localhost/pomodoro/", USER, PASSWORD);
-    
-        } catch (Exception e) {
-           JOptionPane.showMessageDialog(null, "Error de conexion " + e.getMessage());
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion " + ex.getMessage());
+            throw ex;
         }
+
     }
 }
